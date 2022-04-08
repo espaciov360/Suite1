1954c028c6a42d613862f1247f7a9c3dab2dca9f
espaciov360 <espaciov360@gmail.com>
2022-04-08 00:20:47 -0700

    Add project files.

---
+++ Suite1/Controllers/CentroAtencionController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Suite1.Controllers
+{
+    public class CentroAtencionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("CentroAtencion");
+        }
+    }
+}
